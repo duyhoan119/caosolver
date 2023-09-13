e60f6b0f6dd841b9f77e94bc52fecc12440422d9
lo duy hoan <duyhoan3721@gmail.com>
2023-09-13 14:47:59 +0700

    commit 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,25 @@
 module.exports = {
-    content: [
-        "./src/**/*.{html,js}",
-        "./dist/**/*.{html,js}",
-    ],
-    theme: {
-        extend: {
-            
-        },
+  content: ["./src/**/*.{html,js}", "./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "bg-emerald-50": "#e9f3ef",
+        "bg-emerald-400": "#9ceedd",
+        "bg-orange-50": "#fdf6e1",
+        "text-gray-50":"#B3B3B3",
+        "text-gray-100":"#5C5F5E",
+        "bg-violet-50":"#EAEBF6",
+        "bg-amber-50":"#EAEBF6",
+        "bg-emerald-200":"#DDF7EC",
+        "bg-gray-50":"#e9f3ef",
+      },
+      spacing: {
+        38: "152px",
+      },
     },
-    plugins: [
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/line-clamp'),
-    ],
-}
-
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
+  ],
+};
